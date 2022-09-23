85d2a338548a4ea15e51264680a923be9c13f9ed
emanueljg <emanueljohnsongodin@gmail.com>
2022-09-23 17:08:15 +0200

    board fields mark 2

--- app/src/main/java/musicinfo/Band.java
+++ app/src/main/java/musicinfo/Band.java
@@ -1,6 +1,9 @@
+// e - lägger till java.util.*-import. Som thomas sa så är detta lite halvfult
+// men för vårt arbetssätt så passar det bra - det ändrar bara en rad kod!
 class Band {
     public String name;
     public String infoText;
     public int bandStart;
     public int bandEnd;
+    // e - lägger till arists och artistJoinYears
 }
