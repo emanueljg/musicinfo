14fb7ba9f18c31562a3be13e710f27c1e5a1c553
D <dennis.nevback@gmail.com>
2022-09-26 13:13:58 +0200

    D lägger in 5 fält

--- app/src/main/java/musicinfo/Band.java
+++ app/src/main/java/musicinfo/Band.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
-class Band {
+class Band {   // D skriver in de sista 5
     public String name;
     public String infoText;
     public int bandStart;
     public int bandEnd;
     public ArrayList<Artist> artists;
     public HashMap<Integer, Artist> artistJoinYear;
+    public
 }
