c354fcccbcdb809f8c7381d27aab896a662955b0
MaxEk <Max.Ekstedt@gmail.com>
2022-09-26 13:27:06 +0200

    added fields, artis setup

--- app/src/main/java/musicinfo/Artist.java
+++ app/src/main/java/musicinfo/Artist.java
@@ -1,3 +1,19 @@
+package musicinfo;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Artist {
-  
+
+    public String name;
+    public String infoText;
+    public int birthYear;
+    public ArrayList bands = new ArrayList<>();
+    public ArrayList oldBands = new ArrayList<>();
+    public ArrayList albums = new ArrayList<>();
+    public HashMap<String, Album> albumInstruments = new HashMap<String, Album>();
+
+
+
+
+
 }
