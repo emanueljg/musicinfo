4ee0aa5cdf08b248a660f37a0d124bba264f112c
MaxEk <Max.Ekstedt@gmail.com>
2022-09-28 13:23:56 +0200

    removed fields band / oldband from ArtistClass

--- app/src/main/java/musicinfo/Artist.java
+++ app/src/main/java/musicinfo/Artist.java
@@ -7,8 +7,6 @@ class Artist {
     public String name;
     public String infoText;
     public int birthYear;
-    public ArrayList<Band> bands = new ArrayList<Band>();
-    public ArrayList<Band> oldBands = new ArrayList<Band>();
     public ArrayList<Album> albums = new ArrayList<Album>();
     public HashMap<String, Album> albumInstruments = new HashMap<String, Album>();
 
