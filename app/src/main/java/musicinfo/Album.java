1a14497c42295603b54ab1bc3caea9ae22e87dd2
MaxEk <Max.Ekstedt@gmail.com>
2022-09-28 15:42:33 +0200

    added releaseYear to the constructor in AlbumClass

--- app/src/main/java/musicinfo/Album.java
+++ app/src/main/java/musicinfo/Album.java
@@ -4,8 +4,9 @@ class Album extends MusicItem {
     public int releaseYear;
 
 
-    public Album(String name, String info){
+    public Album(String name, String info, int releaseYear){
         super(name, info);
+        this.releaseYear = releaseYear;
 
 
     }
