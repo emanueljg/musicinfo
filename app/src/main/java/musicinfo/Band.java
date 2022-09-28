5997a16d3b5674f77de96990edd154274fd0cb1e
PGamingHD <pontus.2003@hotmail.com>
2022-09-28 15:14:53 +0200

    Merge with Band, getArtistHistory added. Removed 2 variables, added LinkedHashMap artistHistories.

--- app/src/main/java/musicinfo/Band.java
+++ app/src/main/java/musicinfo/Band.java
@@ -1,20 +1,25 @@
 package  musicinfo;
 import java.util.*;
 
-class Band extends MusicItem{
+class Band extends MusicItem {
     public int bandStart;
     public int bandEnd;
     public ArrayList<Artist> artists;
-    public HashMap<Integer, Artist> artistJoinYear;
     public HashMap<String, Artist> artistInstruments;
-    public ArrayList<Artist> oldArtist;
-    public HashMap<Integer, Artist> oldArtistLeaveYears;
+    public LinkedHashMap<Artist, Integer> artistHistories;
     public ArrayList<Album> albums;
 
-    public Band(String name, String info){
+    public Band(String name, String info) {
         super(name, info);
-
-
     }
 
+    public ArrayList<Integer> getArtistHistory(Artist artist) {
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        for (Map.Entry<Artist, Integer> i : artistHistories.entrySet()) {
+            if (i.getKey().equals(artist)) {
+                values.add(i.getValue());
+            }
+        }
+        return values;
+    }
 }
