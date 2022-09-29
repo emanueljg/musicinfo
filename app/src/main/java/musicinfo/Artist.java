d7fda58162f16c246cb59f16f62e02c2b8b0c9c5
D <dennis.nevback@gmail.com>
2022-09-29 10:36:00 +0200

    Added setInstrument method

--- app/src/main/java/musicinfo/Artist.java
+++ app/src/main/java/musicinfo/Artist.java
@@ -6,15 +6,17 @@ class Artist extends MusicItem {
 
     public int birthYear;
     public ArrayList<Album> albums = new ArrayList<Album>();
-    public HashMap<String, Album> albumInstruments = new HashMap<String, Album>();
+    public HashMap<Album, String> albumInstruments = new HashMap<Album, String>();
+
 
     public Artist(String name, String info){
         super(name, info);
 
     }
 
-
-
+    public void setInstrument(Album album, String instruments){
+        albumInstruments.put(album, instruments);
+    }
 
 
 }
