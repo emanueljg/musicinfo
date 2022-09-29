651d4b84170e40108aa8f96a0abda2ebeb8bb884
MaxEk <Max.Ekstedt@gmail.com>
2022-09-29 11:04:00 +0200

    made method removeAlbum in class Album

--- app/src/main/java/musicinfo/Album.java
+++ app/src/main/java/musicinfo/Album.java
@@ -10,12 +10,14 @@ class Album extends MusicItem {
     public Album(String name, String info, int releaseYear){
         super(name, info);
         this.releaseYear = releaseYear;
-
-
     }
 
     @Override
     public ArrayList<MusicItem> getItems(){
         return ALBUMS;
+    }
+
+    public void removeAlbum(int i) {
+        ALBUMS.remove(i);
     }
 }
